83d3a74b4b3be4e102b8155e6207d8b60d58be07
KG <honda1340916@gmail.com>
2022-02-02 23:51:17 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,110 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import { Alert, StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
+import React, {useState} from 'react';
+
+import Status from './components/Status';
+import {
+  createTextMessage,
+  createImageMessage,
+  createLocationMessage
+} from './utils/MessageUtils'
+import MessageList from './components/MessageList';
 
 export default function App() {
+  const initialMessages = [
+    createImageMessage('https://unsplash.it/300/300'),
+    createTextMessage('World'),
+    createTextMessage('Hello'),
+    createLocationMessage({
+      latitude: 37.78825,
+      longitude: -122.4324,
+    })
+  ]
+
+  const [messages, setMessages] = useState(initialMessages)
+  const [fullscreenImageId, setFullscreenImageId] = useState(null)
+
+  const renderMessageList = () => {
+    return (
+      <View style={styles.content}>
+        <MessageList
+          messages={messages}
+          onPressMessage={handlePressMessage}
+        />
+      </View>
+    )
+  }
+
+  const handlePressMessage = ({id, type}) => {
+    switch (type) {
+      case 'text':
+        Alert.alert(
+          'Delete message?',
+          'Are you sure you want to permanently delete this message?',
+          [
+            {
+              text: 'Cancel',
+              style: 'cancel'
+            },
+            {
+              text: 'Delete',
+              style: 'destructive',
+              onPress: () => {
+                const updatedMessages = messages.filter(message => message.id !== id)
+                setMessages(updatedMessages)
+              }
+            }
+          ]
+        )
+        break
+      case 'image':
+        setFullscreenImageId(id)
+        break
+      default:
+        break
+    }
+  }
+
+  const dismissFullscreenImage = () => {
+    setFullscreenImageId(null)
+  }
+
+  const renderFullscreenImage = () => {
+    if (!fullscreenImageId) return null
+
+    const image = messages.find(message => message.id === fullscreenImageId)
+
+    if (!image) return null
+
+    const {uri} = image
+
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={dismissFullscreenImage}
+      >
+        <Image style={styles.fullscreenImage} source={{uri}} />
+      </TouchableHighlight>
+    )
+  }
+
+  const renderInputMethodEditor = () => {
+    return (
+      <View style={styles.inputMethodEditor}></View>
+    )
+  }
+  const renderToolbar = () => {
+    return (
+      <View style={styles.toolbar}></View>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Status />
+      {renderMessageList()}
+      {renderToolbar()}
+      {renderInputMethodEditor()}
+      {renderFullscreenImage()}
     </View>
   );
 }
@@ -13,8 +112,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  content: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: 'white',
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+    zIndex: 2
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: 'contain',
+  }
 });
---
+++ utils/MessageUtils.js
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types'
+
+const MessageShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  type: PropTypes.oneOf(['text', 'image', 'location']),
+  text: PropTypes.string,
+  uri: PropTypes.string,
+  coordinate: PropTypes.shape({
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+  })
+})
+
+let messageId = 0
+
+const getNextId = () => {
+  messageId += 1
+  return messageId
+}
+
+export const createTextMessage = (text) => {
+  return {
+    type: 'text',
+    id: getNextId(),
+    text,
+  }
+}
+
+export const createImageMessage = (uri) => {
+  return {
+    type: 'image',
+    id: getNextId(),
+    uri,
+  }
+}
+
+export const createLocationMessage = (coordinate) => {
+  return {
+    type: 'location',
+    id: getNextId(),
+    coordinate,
+  }
+}
+
+export default MessageShape
